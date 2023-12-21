dc15699e590c621de2de03744c53853aceecd14b
healthtr <154490964+healthtr@users.noreply.github.com>
2023-12-21 12:18:52 +0530

    Update REMINDER.py

--- REMINDER.py
+++ REMINDER.py
@@ -32,3 +32,41 @@ if __name__ == "__main__":
     # Setting reminder
     reminder_app.set_reminder("Aspirin", time.time())
     reminder_app.set_reminder("Ibuprofen", time.time())
+
+
+class Doctor:
+    def _init_(self, name, speciality):
+        self.name = name
+        self.speciality = speciality
+        self.patient_records = {}
+
+    def upload_details(self, patient_name, details):
+        if patient_name not in self.patient_records:
+            self.patient_records[patient_name] = details
+            print(f"Details uploaded for {patient_name} by Dr. {self.name}")
+        else:
+            print(f"Details for {patient_name} already exist.")
+
+    def view_patient_details(self, patient_name):
+        if patient_name in self.patient_records:
+            return self.patient_records[patient_name]
+        else:
+            return f"No details found for {patient_name}"
+
+class Patient:
+    def _init_(self, name):
+        self.name = name
+    def follow_doctor(self, doctor, details):
+        doctor.upload_details(self.name, details)
+    def view_details(self, doctor):
+        return doctor.view_patient_details(self.name)
+if __name__ == "_main_":
+    # Creating a doctor
+    doctor1 = Doctor("Smith", "Cardiologist")
+    # Creating a patient
+    patient1 = Patient("Alice")
+    # Patient follows the doctor and uploads details
+    patient1.follow_doctor(doctor1, "Heart condition, medication prescribed.")
+    # Patient views details
+    details = patient1.view_details(doctor1)
+    print(f"Patient {patient1.name} details: {details}")
